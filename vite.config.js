a661899e8426d7fdcd15b06c03b34509ea582358
Muhammed Safvan MP <134296048+MuhammedSafvanMP@users.noreply.github.com>
2025-05-09 10:50:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: 7868,
-    allowedHosts: ['e-learning-dashboard.onrender.com'], 
+    allowedHosts: ['admin.cognixlearn.com'], 
   },
   resolve: {
     alias: {
